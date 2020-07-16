4964a201b1eacc2893755da4e75251ddc128f859
Mc Nabb <dabl16005@gmail.com>
2020-07-16 01:24:54 -0600

    iconos a botones

--- HBV/HBV/settings.py
+++ HBV/HBV/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hbvdb',
+        'NAME': 'hbvdb2',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'admin01',
         'HOST': 'localhost',
         'PORT': 5432,
     }
