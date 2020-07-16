509d71c42e7f31d986f6557178115bf98f2b2f41
Mc Nabb <dabl16005@gmail.com>
2020-07-16 13:21:40 -0600

    TODO FUNCIONAL

--- HBV/encuesta/urls.py
+++ HBV/encuesta/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url,include
-from encuesta.views import CategoriaList, CategoriaCreate, CategoriaUpdate, CategoriaDelete, PreguntaList, PreguntaDelete, PreguntaCreate, PreguntaUpdate, PreguntaDetail
+from encuesta.views import CategoriaList, CategoriaCreate, CategoriaUpdate, CategoriaDelete, PreguntaList, PreguntaDelete, PreguntaCreate, PreguntaUpdate, PreguntaDetail, municipiosPorDepartament, responderEncuesta
 from django.contrib.auth.decorators import login_required
-
+from django.urls import path
 
 app_name="encuesta"
 urlpatterns = [
@@ -14,4 +14,6 @@ urlpatterns = [
     url(r'^editar_pre/(?P<pk>\d+)/$',login_required(PreguntaUpdate.as_view()), name='pregunta_editar'),
     url(r'^eliminar_pre/(?P<pk>\d+)/$',login_required(PreguntaDelete.as_view()), name='pregunta_eliminar'),
     url(r'^detalle_pre/(?P<pk>\d+)/$',login_required(PreguntaDetail.as_view()), name='pregunta_detalle'),
+    path('responder-encuesta/', responderEncuesta, name='responder_encuesta'),
+    path('municipios/<id_deparmento>/', municipiosPorDepartament, name='municipios'),
 ]
--- HBV/encuesta/views.py
+++ HBV/encuesta/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+import datetime
 # *******************CLASES********************************
 from django.urls import reverse_lazy
 #libreria para vista de tipo lista de django
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 #*********************************************************
 from encuesta.forms import CategoriaForm, PreguntaForm
-from encuesta.models import Categoria, Pregunta
+from encuesta.models import Categoria, Pregunta, Municipio, Departamento, Encuesta, Respuesta
 # Create your views here.
-
 #Vistas temporales
 
 def entorno_admin(request):
@@ -68,3 +70,65 @@ class PreguntaDetail(DetailView):
 	template_name = 'pregunta/pregunta_detail.htm'
 	success_url = reverse_lazy('encuesta:pregunta_listar')
 #*******************************************************************************************************
+def responderEncuesta(request):
+	departamentos = Departamento.objects.all()
+	categorias = Categoria.objects.all()
+
+	#Si se envía la encuesta
+	if request.method == 'POST':
+		
+		#Se crea una nueva encuesta
+		encuesta = Encuesta()
+		
+		#Se le asignan valores a las variales para el modelo Encuesta
+		fecha = datetime.date.today()
+		hora = datetime.datetime.now().strftime('%H:%M:%S')
+		departamento = Departamento.objects.get(pk=request.POST['departamento'])
+		municipio = Municipio.objects.get(pk=request.POST['municipio'])
+
+		#Se le asignan las variables al modelo encuesta
+		encuesta.fecha = fecha
+		encuesta.hora = hora
+		encuesta.fk_id_departamento = departamento
+		encuesta.fk_id_municipio = municipio
+		
+		#Se registra la encuesta
+		encuesta.save()
+
+		#Se obtienen las preguntas enviadas desde el template
+		preguntas = request.POST.getlist('preguntas')
+
+		for p in preguntas:
+			
+			#Se crea una nueva respuesta
+			respuesta = Respuesta()
+
+			#Asignación de valores a las variables para el modelo Respuesta
+			pregunta = Pregunta.objects.get(pk=p)
+			valor = request.POST['pregunta'+p]
+
+			#Asignación de variables al modelo Respuesta
+			respuesta.valor = valor
+			respuesta.fk_id_encuesta = encuesta
+			respuesta.fk_id_pregunta = pregunta
+
+			#Se registra la respuesta
+			respuesta.save()
+
+			#return redirect('url_app_name:url_name')
+
+	context = {
+		'departamentos' : departamentos,
+		'categorias' : categorias,
+	}
+
+	return render(request, 'encuesta/responder_encuesta.html', context)
+
+def municipiosPorDepartament(request, id_deparmento):
+	municipios = Municipio.objects.filter(fk_id_departamento=id_deparmento)
+	#raise Exception("I want to know the value of this: " + municipios)
+	
+	municipios = Municipio.objects.filter(fk_id_departamento=id_deparmento)
+	municipios_list = serializers.serialize('json', municipios)
+
+	return HttpResponse(municipios_list, content_type="text/json-comment-filtered")
