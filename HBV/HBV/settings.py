e5e580c8b73f8aa0dbda02b7923401aa95909b27
Mc Nabb <dabl16005@gmail.com>
2020-07-16 12:32:38 -0600

    Centre preguntas list

--- HBV/HBV/settings.py
+++ HBV/HBV/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hbvdb',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'admin01',
         'HOST': 'localhost',
         'PORT': 5432,
     }
